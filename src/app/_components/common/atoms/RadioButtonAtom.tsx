8a0626573730b606dd531848dc7db6394345eaa8
gahyeon <gus_05@naver.com>
2024-08-01 05:59:45 +0900

    FF-2-feat radio button 컴포넌트 구현

---
+++ src/app/_components/common/atoms/RadioButtonAtom.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { CheckedRadioButton, UncheckedRadioButton } from "@/app/_assets/icons";
+
+interface RadioButtonAtomProps {
+  isChecked: boolean;
+}
+
+function RadioButtonAtom({ isChecked }: RadioButtonAtomProps) {
+  return (
+    <div>
+      <Image
+        src={isChecked ? CheckedRadioButton : UncheckedRadioButton}
+        alt={isChecked ? "checked" : "unchecked"}
+      />
+    </div>
+  );
+}
+
+export default RadioButtonAtom;
