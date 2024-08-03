ff5daac6dadaeee875fcf7dc87bf12e38e85073f
gahyeon <gus_05@naver.com>
2024-08-03 22:37:28 +0900

    FF-10-feat pagination 관련 아이콘 추가

--- src/app/_assets/icons/index.ts
+++ src/app/_assets/icons/index.ts
@@ -1,2 +1,6 @@
 export { default as CheckedRadioButton } from './checkedRadioButton.svg';
 export { default as UncheckedRadioButton } from './unChekcedRadioButton.svg';
+export { default as ChevronRightEmptyIcon } from './chevronRightEmpty.svg';
+export { default as ChevronLeftEmptyIcon } from './chevronLeftEmpty.svg';
+export { default as ChevronRightBlackIcon } from './chevronRightBlack.svg';
+export { default as ChevronLeftBlackIcon } from './chevronLeftBlack.svg';
