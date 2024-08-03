5c071f84abb0952476d7c309e6a85e3c91f5860d
gahyeon <gus_05@naver.com>
2024-08-04 02:50:08 +0900

    FF-11-feat 아티클 커버 컴포넌트 구현

--- src/app/_assets/icons/index.ts
+++ src/app/_assets/icons/index.ts
@@ -4,3 +4,4 @@ export { default as ChevronRightEmptyIcon } from './chevronRightEmpty.svg';
 export { default as ChevronLeftEmptyIcon } from './chevronLeftEmpty.svg';
 export { default as ChevronRightBlackIcon } from './chevronRightBlack.svg';
 export { default as ChevronLeftBlackIcon } from './chevronLeftBlack.svg';
+export { default as PointScanIcon } from './pointScan.svg';
