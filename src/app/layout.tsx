f33f4f6ccd1ce1bec215e196c05f1051c1989cbc
gahyeon <gus_05@naver.com>
2024-08-04 05:51:16 +0900

    FF-5-feat 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Gowun_Dodum, Noto_Sans_KR } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const notoSansKR = Noto_Sans_KR({ subsets: ['latin'] });
+const gowunDodum = Gowun_Dodum({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--gowun',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="kr" className={`${notoSansKR.className} ${gowunDodum.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
