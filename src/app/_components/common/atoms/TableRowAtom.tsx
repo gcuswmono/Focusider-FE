ba3506313371fe4cdf12809f94fc2fe2702f4bbd
gahyeon <gus_05@naver.com>
2024-08-04 16:46:24 +0900

    FF-18-feat TableRowAtom 생성

---
+++ src/app/_components/common/atoms/TableRowAtom.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+
+interface TableRowAtomProps {
+  children?: ReactNode;
+  fontSize?: string;
+  color?: string;
+  isFirst?: boolean;
+  isLast?: boolean;
+}
+
+const TableRowAtom = ({ children, fontSize, color, isFirst, isLast }: TableRowAtomProps) => {
+  return (
+    <td
+      className={`bg-white text-center ${fontSize || 'text-3'} ${color || ''} border-y border-stroke-100 px-5 ${isFirst ? 'rounded-l-regular border-l pl-8' : ''} ${isLast ? 'rounded-r-regular border-r pr-8' : ''}`}
+    >
+      {children}
+    </td>
+  );
+};
+
+export default TableRowAtom;
