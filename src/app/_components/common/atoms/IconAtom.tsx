89f59067cedaf599b4c8a0db967fa26020021df9
gahyeon <gus_05@naver.com>
2024-08-04 15:58:35 +0900

    FF-13-feat 아이콘 컴포넌트 props에 className 추가

--- src/app/_components/common/atoms/IconAtom.tsx
+++ src/app/_components/common/atoms/IconAtom.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image';
 interface IconAtomProps {
   src: string;
   alt: string;
+  className?: string;
 }
 
-const IconAtom = ({ src, alt }: IconAtomProps) => {
-  return <Image src={src} alt={alt} />;
+const IconAtom = ({ src, alt, className = '' }: IconAtomProps) => {
+  return <Image src={src} alt={alt} className={className} />;
 };
 
 export default IconAtom;
