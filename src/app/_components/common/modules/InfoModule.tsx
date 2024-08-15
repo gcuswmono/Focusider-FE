137e2663723b95c19ee596ae76b8011a071634be
gahyeon <gus_05@naver.com>
2024-08-15 16:15:27 +0900

    FF-24-design InfoModule 패딩 수정

--- src/app/_components/common/modules/InfoModule.tsx
+++ src/app/_components/common/modules/InfoModule.tsx
@@ -9,7 +9,7 @@ interface InfoModuleProps {
 
 const InfoModule = ({ text, src, alt }: InfoModuleProps) => {
   return (
-    <div className="flex w-fit items-center gap-x-1 rounded-full bg-white px-4 py-1.5">
+    <div className="flex w-fit items-center gap-x-1 rounded-full bg-white px-3.5 py-1.5">
       <IconAtom src={src} alt={alt} className="h-5 w-5" />
       <InfoTextAtom text={text} className="text-4 font-semibold" />
     </div>
