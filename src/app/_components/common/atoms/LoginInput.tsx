e99be1dc2f6a5e18e333680221c13c210cc9f229
gahyeon <gus_05@naver.com>
2024-08-15 12:13:16 +0900

    FF-23-feat placeholder 오른쪽 정렬과 width fixed props 추가

--- src/app/_components/common/atoms/LoginInput.tsx
+++ src/app/_components/common/atoms/LoginInput.tsx
@@ -10,6 +10,8 @@ interface UserLoginInputProps {
   error: string | null;
   showDuplicateCheck?: boolean;
   onDuplicateCheckClick?: () => void;
+  placeholderRight?: boolean;
+  fixed?: string;
 }
 
 const LoginInput = ({
@@ -21,27 +23,31 @@ const LoginInput = ({
   error,
   showDuplicateCheck = false,
   onDuplicateCheckClick,
+  placeholderRight = false,
+  fixed = '',
 }: UserLoginInputProps) => {
   return (
     <div className="flex w-full flex-col">
       <div
-        className={`flex h-14 w-full items-center gap-x-2.5 rounded bg-white ${
+        className={`flex h-14 items-center gap-x-2.5 rounded bg-white ${
           error ? 'border-2 border-negative' : ''
-        } px-5 py-2.5`}
+        } px-5 py-2.5 ${fixed || 'grow'}`}
       >
         <input
           type={type}
           name={name}
           value={value}
           onChange={onChange}
-          className="h-full grow text-sub-400 outline-none placeholder:text-sub-200"
+          className={`h-full w-full text-sub-400 outline-none placeholder:text-sub-200 ${
+            placeholderRight ? 'text-right' : ''
+          } `}
           placeholder={placeholder}
         />
         {showDuplicateCheck && (
           <button
             type="button"
             onClick={onDuplicateCheckClick}
-            className="rounded-md border border-sub-200/50 px-3 py-1.5 text-4 text-sub-200"
+            className="flex-shrink-0 rounded-md border border-sub-200/50 px-3 py-1.5 text-4 text-sub-200"
           >
             중복확인
           </button>
