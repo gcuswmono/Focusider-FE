3f4231593d11ea2031b4512b8f2f7afa4c5c22b6
gahyeon <gus_05@naver.com>
2024-08-15 13:48:52 +0900

    FF-23-feat 맞춤형 콘텐츠 설정 관련 페이지 적용

--- src/app/_components/signup/SignUpContainer.tsx
+++ src/app/_components/signup/SignUpContainer.tsx
@@ -3,6 +3,10 @@
 import CreateAccountForm from '@/app/_components/signup/CreateAccountForm';
 import UserProfileForm from '@/app/_components/signup/UserProfileForm';
 import PersonalizationSettings from '@/app/_components/signup/PersonalizationSettings';
+import FirstQuestionPage from '@/app/_components/signup/FirstQuestion';
+import SecondQuestionPage from '@/app/_components/signup/SecondQuestion';
+import ThirdQuestionPage from '@/app/_components/signup/ThirdQuestion';
+import CompletionPage from '@/app/_components/signup/CompletionPage';
 
 interface UserinfoPageComponentsProps {
   slug: string;
@@ -17,6 +21,14 @@ const SignUpContainer = ({ slug }: UserinfoPageComponentsProps) => {
       return <UserProfileForm pageNum={pageNum} />;
     case '3':
       return <PersonalizationSettings pageNum={pageNum} />;
+    case '4':
+      return <FirstQuestionPage currentQuestion={0} totalQuestions={3} pageNum={pageNum} />;
+    case '5':
+      return <SecondQuestionPage currentQuestion={1} totalQuestions={3} pageNum={pageNum} />;
+    case '6':
+      return <ThirdQuestionPage currentQuestion={2} totalQuestions={3} pageNum={pageNum} />;
+    case '7':
+      return <CompletionPage />;
     default:
       return '';
   }
