c6c7c6fe83e3b13744a60fb68b9b59f2c9e0a323
gahyeon <gus_05@naver.com>
2024-08-15 12:21:54 +0900

    FF-23-feat 프로필 사진 설정

--- src/app/_components/signup/UserProfileForm.tsx
+++ src/app/_components/signup/UserProfileForm.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import LoginInput from '@/app/_components/common/atoms/LoginInput';
 import ButtonAtom from '@/app/_components/common/atoms/ButtonAtom';
+import Image from 'next/image';
+import { AddProfileIcon, EmptyProfileIcon } from '@/app/_assets/icons';
 
 interface Props {
   pageNum: string;
@@ -11,6 +13,8 @@ interface Props {
 
 const UserProfileForm = ({ pageNum }: Props) => {
   const router = useRouter();
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+
   const [form, setForm] = useState({
     name: '',
     year: '',
@@ -40,6 +44,17 @@ const UserProfileForm = ({ pageNum }: Props) => {
     });
   };
 
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setProfileImage(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleSignup = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // 프로필 생성 로직 추가
@@ -50,7 +65,38 @@ const UserProfileForm = ({ pageNum }: Props) => {
     <section className="flex w-full items-center justify-center bg-primary-100">
       <div className="flex h-dvh w-[440px] flex-col items-center justify-center gap-y-6">
         <h1 className="w-full text-h3 font-bold">프로필 설정</h1>
-        <form className="flex w-full flex-col gap-y-10" onSubmit={handleSignup}>
+        <form className="flex w-full flex-col gap-y-8" onSubmit={handleSignup}>
+          <div className="flex justify-center">
+            <div className="relative w-fit justify-center">
+              {profileImage ? (
+                <Image
+                  src={profileImage}
+                  alt="미리 보기"
+                  className="h-32 w-32 rounded-full object-cover"
+                />
+              ) : (
+                <Image
+                  src={EmptyProfileIcon}
+                  alt="profile"
+                  className="h-32 w-32 rounded-full object-cover text-gray-300"
+                />
+              )}
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label
+                htmlFor="profileImage"
+                className="absolute bottom-0 right-4 translate-x-2 translate-y-2 transform cursor-pointer"
+              >
+                <Image src={AddProfileIcon} alt="Add profile" className="h-12 w-12 rounded-full" />
+              </label>
+            </div>
+            <input
+              type="file"
+              id="profileImage"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+          </div>
           <div className="flex w-full flex-col gap-y-2">
             <div className="flex items-center justify-between px-1">
               <p className="font-semibold">이름</p>
