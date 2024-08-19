072ed52ce6df1c63d06d3d6bc1ae0631ce1762ca
gahyeon <gus_05@naver.com>
2024-08-19 14:18:20 +0900

    FF-26-feat 아티클 모아보기 페이지 아이콘 추가

--- src/app/_assets/icons/index.ts
+++ src/app/_assets/icons/index.ts
@@ -25,3 +25,4 @@ export { default as QuizCoverIcon } from './quizCover.svg';
 export { default as ReportCoverIcon } from './reportCover.svg';
 export { default as CalendarIcon } from './calendar.svg';
 export { default as PeopleIcon } from './people.svg';
+export { default as LightIcon } from './light.svg';
