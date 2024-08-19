2a66d796c91bc2cf33fd3d2569d8365e4614ed77
gahyeon <gus_05@naver.com>
2024-08-19 15:08:06 +0900

    FF-26-fix FontSizeControllerModule에 title props 추가

--- src/app/_components/common/modules/FontSizeControllerModule.tsx
+++ src/app/_components/common/modules/FontSizeControllerModule.tsx
@@ -6,29 +6,32 @@ import { DensityLargeIcon, PlusMathIcon } from '@/app/_assets/icons';
 import Image from 'next/image';
 
 interface FontSizeControllerProps {
+  title: string;
   initialSize: number;
   children: ReactNode;
 }
 
-const FontSizeControllerModule: React.FC<FontSizeControllerProps> = ({ initialSize, children }) => {
+const FontSizeControllerModule = ({ title, initialSize, children }: FontSizeControllerProps) => {
   const [fontSize, setFontSize] = useState(initialSize);
 
-  const increaseFontSize = () => setFontSize(fontSize + 2);
-  const decreaseFontSize = () => setFontSize(fontSize - 2);
+  const increaseFontSize = () => setFontSize((prevSize) => prevSize + 2);
+  const decreaseFontSize = () => setFontSize((prevSize) => prevSize - 2);
 
   return (
-    <div className="flex w-full items-center justify-between p-4">
-      <div className="" style={{ fontSize: `${fontSize}px` }}>
-        {children}
+    <div className="flex w-full flex-col px-10">
+      <div className="flex justify-between">
+        <p className="text-h4 font-semibold">{title}</p>
+        <div className="mb-2 flex justify-end">
+          <FontSizeControllerAtom className="rounded-l-md border-r-0" onClick={increaseFontSize}>
+            <Image src={PlusMathIcon} alt="Increase Font Size" />
+          </FontSizeControllerAtom>
+          <FontSizeControllerAtom className="rounded-r-md" onClick={decreaseFontSize}>
+            <Image src={DensityLargeIcon} alt="Decrease Font Size" />
+          </FontSizeControllerAtom>
+        </div>
       </div>
-      <div className="mt-4 flex">
-        <FontSizeControllerAtom className="rounded-l-md border-r-0" onClick={increaseFontSize}>
-          <Image src={PlusMathIcon} alt="+" />
-        </FontSizeControllerAtom>
-        <FontSizeControllerAtom className="rounded-r-md" onClick={decreaseFontSize}>
-          <Image src={DensityLargeIcon} alt="-" />
-        </FontSizeControllerAtom>
-      </div>
+
+      <div style={{ fontSize: `${fontSize}px` }}>{children}</div>
     </div>
   );
 };
