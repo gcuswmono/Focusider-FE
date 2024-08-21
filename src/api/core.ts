0ad8f40582886026f7a3f3c6d1e1afdab35d006b
mango0422_mac <syj0422@gachon.ac.kr>
2024-08-21 18:36:23 +0900

    안쓰는 코드 주석처리 (추후 사용가능성 염두)

--- src/api/core.ts
+++ src/api/core.ts
@@ -29,7 +29,7 @@ export const HTTP_METHODS: Record<
 
 export const ACCESS_TOKEN = 'accessToken' as const;
 export const REFRESH_TOKEN = 'refreshToken' as const;
-export const ROLE = 'role' as const;
+// export const ROLE = 'role' as const;
 
 axiosInstance.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
