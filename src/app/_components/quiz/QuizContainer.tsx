fb0ac0d989abdc5fa23dc9240f5892911a85ab85
gahyeon <gus_05@naver.com>
2024-08-24 01:16:57 +0900

    FF-33-feat 퀴즈 종료 시 컨페티 애니메이션 적용

--- src/app/_components/quiz/QuizContainer.tsx
+++ src/app/_components/quiz/QuizContainer.tsx
@@ -2,10 +2,11 @@
 
 import QuizInfoContainer from '@/app/_components/quiz/QuizInfoContainer';
 import ButtonAtom from '@/app/_components/common/atoms/ButtonAtom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { ClapIcon } from '@/app/_assets/icons';
+import confetti from 'canvas-confetti';
 
 interface UserinfoPageComponentsProps {
   slug: string;
@@ -14,6 +15,42 @@ interface UserinfoPageComponentsProps {
 const SignUpContainer = ({ slug }: UserinfoPageComponentsProps) => {
   const pageNum = slug[0];
   const router = useRouter();
+
+  const duration = 3 * 1000;
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 20, spread: 300, ticks: 100, zIndex: 0 };
+
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const handleConfetti = () => {
+    const intervalId: NodeJS.Timeout = setInterval(() => {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(intervalId);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 500);
+  };
+
+  useEffect(() => {
+    if (pageNum !== '1' && pageNum !== '2' && pageNum !== '3') {
+      handleConfetti();
+    }
+  }, [pageNum]);
 
   switch (pageNum) {
     case '1':
