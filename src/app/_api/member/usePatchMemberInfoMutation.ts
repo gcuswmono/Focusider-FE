62ec512a4248feec8fd92fda0d59a2940f5c4564
gahyeon <gus_05@naver.com>
2024-08-24 17:44:00 +0900

    FF-34-feat usePatchMemberInfoMutation 생성

---
+++ src/app/_api/member/usePatchMemberInfoMutation.ts
@@ -0,0 +1,41 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { BaseResponse } from '@/api';
+import { useGetMemberInfoQueryKey } from '@/app/_api/member/useGetMemberInfoQuery';
+import api from '../Axios';
+
+interface PatchMemberInfoRequest {
+  profileImageUrl: string;
+  name: string;
+}
+
+export const usePatchMemberInfoMutation = ({
+  successCallback,
+  errorCallback,
+}: {
+  successCallback?: () => void;
+  errorCallback?: (error: Error) => void;
+}) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (request: PatchMemberInfoRequest) => {
+      const response = await api.patch<BaseResponse<void>>('/api/member', request);
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [useGetMemberInfoQueryKey],
+      });
+
+      queryClient.refetchQueries({
+        queryKey: [useGetMemberInfoQueryKey],
+      });
+
+      successCallback && successCallback();
+    },
+    onError: (error: Error) => {
+      console.error('Failed to patch member info:', error);
+      errorCallback && errorCallback(error);
+    },
+  });
+};
