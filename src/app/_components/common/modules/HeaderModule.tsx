6209c4301109b9433d06f610e0ba985a05295ff1
gahyeon <gus_05@naver.com>
2024-08-24 19:11:58 +0900

    FF-35-feat useLogoutMutation 적용

--- src/app/_components/common/modules/HeaderModule.tsx
+++ src/app/_components/common/modules/HeaderModule.tsx
@@ -4,9 +4,20 @@ import { useRouter } from 'next/navigation';
 import logo from '@/app/_assets/images/img_1.png';
 import IconAtom from '@/app/_components/common/atoms/IconAtom';
 import { LogoutIcon } from '@/app/_assets/icons';
+import { useLogoutMutation } from '@/app/_api/auth/useLogoutMutation';
 
 const HeaderModule = () => {
   const router = useRouter();
+
+  const { mutate: logout } = useLogoutMutation({
+    successCallback: () => {
+      router.push('/');
+    },
+    errorCallback: (error) => {
+      console.error('로그아웃 실패:', error);
+    },
+  });
+
   return (
     <div className="flex items-center justify-between border-b border-stroke-100 bg-primary-100 px-[54px] py-3.5">
       <div
@@ -25,7 +36,7 @@ const HeaderModule = () => {
       <div
         role="presentation"
         className="flex items-center justify-between"
-        onClick={() => router.push('/')} // 로그아웃 로직으로 수정 필요
+        onClick={() => logout()}
       >
         <IconAtom src={LogoutIcon.src} alt="logout" width={28} height={28} />
       </div>
