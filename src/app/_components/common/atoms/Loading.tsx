41c9a42549ccacac9b31be288129f30ed3b4fbfb
gahyeon <gus_05@naver.com>
2024-08-24 15:10:38 +0900

    FF-33-design 로딩 컴포넌트 z-index 수정

--- src/app/_components/common/atoms/Loading.tsx
+++ src/app/_components/common/atoms/Loading.tsx
@@ -1,8 +1,8 @@
 import { BeatLoader } from 'react-spinners';
 
-const Loading = () => {
+const Loading = ({ screen = false }) => {
   return (
-    <div className="flex h-full w-full">
+    <div className="fixed inset-0 z-40 flex h-full w-full">
       <BeatLoader color="#A390F2" className="mx-auto my-auto" />
     </div>
   );
