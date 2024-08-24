c2cd172e66f21a8a711a4a7d9b088fda1e3e1cbe
gahyeon <gus_05@naver.com>
2024-08-24 12:33:30 +0900

    FF-33-feat 로딩 컴포넌트 생성

---
+++ src/app/_components/common/atoms/Loading.tsx
@@ -0,0 +1,11 @@
+import { BeatLoader } from 'react-spinners';
+
+const Loading = () => {
+  return (
+    <div className="flex h-full w-full">
+      <BeatLoader color="#A390F2" className="mx-auto my-auto" />
+    </div>
+  );
+};
+
+export default Loading;
