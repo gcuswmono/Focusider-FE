db3817b07a62e8537b42b731c4b23b4abc46fcc5
gahyeon <gus_05@naver.com>
2024-08-26 02:27:24 +0900

    FF-38-feat 아티클 읽기 완료 모달 추가

--- src/app/(route)/(with-layout)/article/layout.tsx
+++ src/app/(route)/(with-layout)/article/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import React, { ReactNode } from 'react';
-import { usePathname } from 'next/navigation';
+import React, { ReactNode, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import NavBarContainer from '@/app/_components/common/containers/NavBarContainer';
-import HeaderModule from '@/app/_components/common/modules/HeaderModule';
+import HeaderNextModule from '@/app/_components/common/modules/HeaderNextModule';
+import ModalModule from '@/app/_components/common/modules/ModalModule';
+import { ClapIcon } from '@/app/_assets/icons';
 
 interface Props {
   children: ReactNode;
@@ -11,6 +13,8 @@ interface Props {
 
 const CommonLayout = ({ children }: Props) => {
   const pathname = usePathname();
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
 
   const isNestedRoute = pathname.split('/').length > 2;
   const isQuizRoute = pathname === '/quiz';
@@ -23,11 +27,26 @@ const CommonLayout = ({ children }: Props) => {
     );
   }
 
+  const handleNextButtonClick = () => {
+    setShowModal(true);
+    setTimeout(() => {
+      setShowModal(false);
+      router.push('/article/chat');
+    }, 3000); // 3초 후에 라우팅
+  };
+
   return (
     <div className="flex h-screen flex-col">
-      <HeaderModule />
+      <HeaderNextModule onClick={handleNextButtonClick} />
       <main className="flex-grow">{children}</main>
       <NavBarContainer />
+      {showModal && (
+        <ModalModule
+          iconSrc={ClapIcon}
+          iconAlt="Icon"
+          title={'읽기가 끝났습니다! \n이제 여러분의 생각을 나눠보세요'}
+        />
+      )}
     </div>
   );
 };
--- src/app/_components/common/atoms/ModalTitleAtom.tsx
+++ src/app/_components/common/atoms/ModalTitleAtom.tsx
@@ -5,7 +5,7 @@ interface ModalTitleAtomProps {
 }
 
 const ModalTitleAtom = ({ title }: ModalTitleAtomProps) => {
-  return <div className="w-full text-center text-1 font-bold">{title}</div>;
+  return <div className="w-full whitespace-pre-line text-center text-1 font-bold">{title}</div>;
 };
 
 export default ModalTitleAtom;
